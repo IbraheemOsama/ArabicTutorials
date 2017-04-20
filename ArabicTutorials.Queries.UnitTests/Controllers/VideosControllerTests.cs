07e195d0951f17128c99d5181d7918dc1f786d45
DESKTOP-QJU4N0L\mityh <ibraheem.osama@gmail.com>
2017-04-20 13:29:18 +1000

    moving folder structure and adding first unit test

---
+++ ArabicTutorials.Queries.UnitTests/Controllers/VideosControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArabicTutorials.Common;
+using ArabicTutorials.Queries.Controllers;
+using NSubstitute;
+using Xunit;
+
+namespace ArabicTutorials.Queries.UnitTests.Controllers
+{
+    public class VideosControllerTests
+    {
+        //[UnitOfWorkName]_[ScenarioUnderTest]_[ExpectedBehavior]
+        [Fact]
+        public void GetAll_ReturnResults()
+        {
+            // Arrange
+            var logger = Substitute.For<ILogger>();
+            var controller = new VideosController(logger);
+
+            // Act
+            var result = controller.GetAll();
+
+            //Assert
+            Assert.NotEqual(0, result.Count());
+        }
+    }
+}
